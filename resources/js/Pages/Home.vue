c83ece7d783940947da2fa9fcdbe3b75fc854f5f
Musa Demir <alpermusademir@gmail.com>
2022-01-13 22:11:58 +0300

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 class="text-4xl font-bold">Home</h1>
+
+    <Nav />
+</template>
+
+<script>
+import Nav from '../Shared/Nav';
+
+export default {
+    components: {
+        Nav
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @inertiaHead
+</head>
+<body>
+    <section class="p-8">
+        @inertia
+    </section>
+</body>
+</html>
