c2e1e34bd3b90ee9982309396ead565794180115
Musa Demir <alpermusademir@gmail.com>
2022-01-16 18:46:44 +0300

    Ep 9, Layout Files

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,26 +1,26 @@
 <template>
-    <h1 class="text-4xl font-bold">Users</h1>
+    <Layout>
+        <h1 class="text-3xl">Users</h1>
 
-    <Nav />
+        <div style="margin-top: 800px">
+            <p>The current time is {{ time }}.</p>
 
-    <div style="margin-top: 800px">
-        <p>The current time is {{ time }}.</p>
-
-        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-    </div>
+            <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+        </div>
+    </Layout>
 </template>
 
 <script>
-import Nav from '../Shared/Nav';
+import Layout from '../Shared/Layout';
 import { Link } from '@inertiajs/inertia-vue3';
 
 export default {
     components: {
-        Nav,
+        Layout,
         Link
     },
     props: {
-        time: String,
+        time: String
     }
 }
 </script>
