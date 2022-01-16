906e56a34c12af677cbaba2baa866452e1f93ed4
Musa Demir <alpermusademir@gmail.com>
2022-01-16 19:11:36 +0300

    Ep 11, Global Component Registration

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -12,12 +12,10 @@
 
 <script>
 import Layout from '../Shared/Layout';
-import { Link } from '@inertiajs/inertia-vue3';
 
 export default {
     components: {
         Layout,
-        Link
     },
     props: {
         time: String
