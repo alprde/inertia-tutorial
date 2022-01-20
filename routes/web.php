b380fcbb1c616b9ff4a15e3da1349affdcf7b745
Musa Demir <alpermusademir@gmail.com>
2022-01-20 19:45:58 +0300

    Ep 17, Pagination

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ Route::get('/', function () {
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
-       'users' => User::all()->map(fn($users) => [
+       'users' => User::paginate(10)->through(fn($users) => [
+           'id' => $users->id,
            'name' => $users->name
        ])
     ]);
